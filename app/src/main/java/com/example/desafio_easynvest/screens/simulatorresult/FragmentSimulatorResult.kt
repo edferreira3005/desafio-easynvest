0f6783305ec9533f46abe7e9f0b864da83511f06
Edson Ferreira <edferreira3005@gmail.com>
2020-03-07 17:37:43 -0300

    adicionando valores para os textview da tela de resultado (inicio)

--- app/src/main/java/com/example/desafio_easynvest/screens/simulatorresult/FragmentSimulatorResult.kt
+++ app/src/main/java/com/example/desafio_easynvest/screens/simulatorresult/FragmentSimulatorResult.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.desafio_easynvest.R
+import com.example.desafio_easynvest.model.ResponseSimulator
 import kotlinx.android.synthetic.main.layout_fragment_simulator_result.*
 
 class FragmentSimulatorResult : Fragment() {
@@ -18,6 +19,13 @@ class FragmentSimulatorResult : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        var responseSimulator: ResponseSimulator? = null
+        if(arguments != null)
+            responseSimulator = arguments!!.getSerializable("response") as ResponseSimulator?
+
+        if(responseSimulator != null)
+            setFields(responseSimulator)
+
         btnSumularNovamente.setOnClickListener {
             val onClick = activity as OnClickSimulateAgain
             onClick.onClickSimulateAgain()
@@ -26,5 +34,26 @@ class FragmentSimulatorResult : Fragment() {
 
     interface OnClickSimulateAgain {
         fun onClickSimulateAgain()
+    }
+
+    private fun setFields(responseSimulator: ResponseSimulator) {
+
+        if(responseSimulator.investmentParameter != null) {
+            tvValAplicadoInicial.text = responseSimulator.investmentParameter!!.investedAmount.toString()
+            tvValDataResgate.text = responseSimulator.investmentParameter!!.maturityDate
+            tvValDiasCorridos.text = responseSimulator.investmentParameter!!.maturityTotalDays.toString()
+            tvValPercentualCDI.text = responseSimulator.investmentParameter!!.rate.toString()
+
+        }
+
+        tvValBruto.text = responseSimulator.grossAmount.toString()
+        tvRendimentoTotal.text = tvRendimentoTotal.text.toString().replace("__VALOR",responseSimulator.grossAmountProfit.toString())
+        tvValResultado.text = responseSimulator.grossAmount.toString()
+        tvValRendimento.text = responseSimulator.grossAmountProfit.toString()
+        tvValIRRendimento.text = responseSimulator.taxesAmount.toString() + "[" + responseSimulator.taxesRate.toString() + "]"
+        tvValLiquido.text = responseSimulator.netAmount.toString()
+        tvValRendimentoMensal.text = responseSimulator.monthlyGrossRateProfit.toString()
+        tvValRentabilidadeAnual.text = responseSimulator.monthlyGrossRateProfit.toString()
+        tvValRentabilidadePeriodo.text = responseSimulator.rateProfit.toString()
     }
 }
